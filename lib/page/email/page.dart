e688ed3d874564949f094cf2bc46063b5b4c9072
Moinak Majumdar <moinak2030@gmail.com>
2024-06-16 12:21:14 +0530

    email reply settings

--- lib/page/email/page.dart
+++ lib/page/email/page.dart
@@ -1,106 +1,142 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio/utils/on_error.dart';
 import 'package:portfolio/utils/on_load.dart';
 import 'package:portfolio/widget/neumorphism.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class Email extends StatefulWidget {
+class Email extends StatelessWidget {
   const Email({super.key});
 
   @override
-  State<Email> createState() => _EmailState();
-}
-
-class _EmailState extends State<Email> {
-  final dbStream = FirebaseFirestore.instance
-      .collection('Email')
-      .orderBy('time', descending: true)
-      .snapshots();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    final dbStream = FirebaseFirestore.instance
+        .collection('Email')
+        .orderBy('time', descending: true)
+        .snapshots();
+
     return Scaffold(
       appBar: AppBar(title: const Text("Client Messages")),
       body: Scrollbar(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: StreamBuilder(
-              stream: dbStream,
-              builder: (ctx, snapShot) {
-                if (snapShot.hasError) {
-                  return OnError(error: snapShot.error.toString());
-                }
-                if (snapShot.hasData) {
-                  return ListView(
-                    children: snapShot.data!.docs.map(
-                      (DocumentSnapshot doc) {
-                        final data = doc.data()! as Map<String, dynamic>;
-                        final dateTime = getVisibleDate(data['time']);
+            stream: dbStream,
+            builder: (ctx, snapShot) {
+              if (snapShot.hasError) {
+                return OnError(error: snapShot.error.toString());
+              }
 
-                        return GestureDetector(
-                          onTap: () async {
-                            await _launchUrl(data["email"], data['sub']);
-                          },
-                          child: NeuBox(
-                            margin: const EdgeInsets.all(12),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '${data["name"]}  <${data["email"]}>',
-                                  style: GoogleFonts.montserrat().copyWith(
-                                    color: Colors.grey.shade300,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w600,
+              if (snapShot.hasData) {
+                return ListView(
+                  children: snapShot.data!.docs.map(
+                    (DocumentSnapshot doc) {
+                      final data = doc.data()! as Map<String, dynamic>;
+                      final dateTime = getVisibleDate(data['time']);
+
+                      return NeuBox(
+                        margin: const EdgeInsets.all(14),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: TextSpan(
+                                style: textTheme.titleMedium,
+                                children: [
+                                  TextSpan(
+                                    text: 'Name: ',
+                                    style: TextStyle(
+                                      color: colorScheme.primary,
+                                    ),
                                   ),
-                                ),
-                                Text(
-                                  dateTime,
-                                  style: GoogleFonts.montserrat(),
-                                ),
-                                const SizedBox(height: 12),
-                                Text(
-                                  data['subject'],
-                                  style: GoogleFonts.lato().copyWith(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 18,
-                                    color: Colors.grey.shade200,
-                                  ),
-                                ),
-                                const SizedBox(height: 4),
-                                Text(
-                                  data['message'],
-                                  style: GoogleFonts.lato().copyWith(
-                                    color: Colors.grey.shade200,
-                                  ),
-                                )
-                              ],
+                                  TextSpan(
+                                    text: data['name'],
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                        );
-                      },
-                    ).toList(),
-                  );
-                }
+                            RichText(
+                              text: TextSpan(
+                                style: textTheme.titleMedium,
+                                children: [
+                                  TextSpan(
+                                    text: 'From: ',
+                                    style: TextStyle(
+                                      color: colorScheme.primary,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: data['email'],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Text(
+                              dateTime,
+                              style: textTheme.titleSmall,
+                            ),
+                            const SizedBox(height: 12),
+                            Text(
+                              data['subject'],
+                              style: textTheme.titleMedium!.copyWith(
+                                color: Colors.pink.shade400,
+                              ),
+                            ),
+                            const SizedBox(height: 4),
+                            Text(
+                              data['message'],
+                              style: textTheme.bodyMedium,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              mainAxisSize: MainAxisSize.max,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                ElevatedButton.icon(
+                                  onPressed: () async {
+                                    await _launchUrl(
+                                      data['email'],
+                                      data['name'],
+                                    );
+                                  },
+                                  icon: Icon(
+                                    Icons.reply,
+                                    color: Colors.grey.shade900,
+                                  ),
+                                  label: Text(
+                                    "Reply",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey.shade900,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                  ).toList(),
+                );
+              }
 
-                return const OnLoad(msg: 'Fetching email ...');
-              }),
+              return const OnLoad(msg: 'Fetching email ...');
+            },
+          ),
         ),
       ),
     );
   }
 
-  Future<void> _launchUrl(String email, String subject) async {
+  Future<void> _launchUrl(String email, String name) async {
     final mail = Uri.encodeComponent(email);
-    final sub = Uri.encodeComponent(subject);
-    final url = Uri.parse("mailto:$mail?subject=$sub");
+    final sub = Uri.encodeComponent("Thanks for reaching me");
+    final body = 'Hey $name,\n\n I am Moinak Majumdar, creator of';
+    final url = Uri.parse("mailto:$mail?subject=$sub&body=$body");
     await launchUrl(url);
   }
 
